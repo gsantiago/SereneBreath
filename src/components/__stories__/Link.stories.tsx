4d79d50330ba625bfe9e87944b01e6a9f507bb0b
Guilherme Santiago <guilherme.santiago95@gmail.com>
2023-03-11 14:04:25 -0300

    feat: add Link stories

---
+++ src/components/__stories__/Link.stories.tsx
@@ -0,0 +1,13 @@
+import { Link } from "../Link";
+
+export default {
+  title: "Components/Link",
+  component: Link,
+};
+
+export const Default = {
+  args: {
+    href: "https://serenebreath.com",
+    children: "SereneBreath",
+  },
+};
