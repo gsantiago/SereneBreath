0a09f3b912883fa936537834d5c12c15ad876f87
Guilherme Santiago <guilherme.santiago95@gmail.com>
2023-03-11 11:06:08 -0300

    refactor: remove theme.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./styles/theme.css";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
