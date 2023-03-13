a25a51a1954fa0880bf57b298ec2a86a1117eb46
Guilherme Santiago <guilherme.santiago95@gmail.com>
2023-03-13 16:13:36 -0300

    test: isActive test for ButtonIcon

--- src/components/__tests__/ButtonIcon.test.tsx
+++ src/components/__tests__/ButtonIcon.test.tsx
@@ -1,15 +1,25 @@
 import { render, axe } from "@/test-utils";
-import { ButtonIcon } from "@/components/ButtonIcon";
+import { ButtonIcon, ButtonIconProps } from "@/components/ButtonIcon";
 
-const setup = () =>
+const setup = (props?: Partial<ButtonIconProps>) =>
   render(
-    <ButtonIcon title="Home">
+    <ButtonIcon title="Home" {...props}>
       <span id="home-icon" />
     </ButtonIcon>
   );
 
 test("renders successfuly", () => {
   const { container } = setup();
+  expect(container).toMatchSnapshot();
+});
+
+test("isActive state", () => {
+  const { container } = setup({ isActive: true });
+  expect(container).toMatchSnapshot();
+});
+
+test("bottom position", () => {
+  const { container } = setup({ position: "bottom" });
   expect(container).toMatchSnapshot();
 });
 
