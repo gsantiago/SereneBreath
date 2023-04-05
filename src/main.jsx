23e4e4b0b4d024d94e97b7a005f8f9ff9c943804
Guilherme Santiago <guilherme.santiago95@gmail.com>
2023-04-05 18:42:00 -0300

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { inject } from "@vercel/analytics";
 import App from "./App";
 
 import "./styles/index.css";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
