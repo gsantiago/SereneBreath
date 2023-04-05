3e6b5f44599208846c912b953d68cbd3cc1375da
Guilherme Santiago <guilherme.santiago95@gmail.com>
2023-04-05 20:46:46 -0300

    feat: use Analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 
 import "./styles/index.css";
 
-inject();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
